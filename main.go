282a21122c1284a0694c886b0dc7eca06be3a6d5
Mandar Tannu <mandartannu19@gmail.com>
2026-01-13 16:19:06 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// DATABASE
+var db *sql.DB
+
+func initDB() {
+	var err error
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	if dbHost == "" || dbPort == "" || dbUser == "" || dbPassword == "" || dbName == "" {
+		log.Fatal("Database environment variables are not set")
+	}
+
+	dsn := "host=" + dbHost +
+		"port=" + dbPort +
+		"user+" + dbUser +
+		"password=" + dbPassword +
+		"dbname=" + dbName +
+		"sslmode=disable"
+
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Failed to open database connection:", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+
+	log.Println("Connected to PostgreSQL successfully")
+
+	createTable()
+}
+
+func createTable() {
+	query := `
+	CREATE TABLE IF NOT EXISTS users(
+		id SERIAL PRIMARY KEY,
+		name TEXT NOT NULL,
+		email TEXT NOT NULL,
+		phone TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
+		`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal("Failed to create table:", err)
+	}
+}
+
+// HTTP HANDLERS
+// SERVES THE HTML FORM
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(w, r, "index.html")
+}
+
+// HANDELS FORM SUBMISSION
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Failed to parse form", http.StatusBadRequest)
+		return
+	}
+
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	phone := r.FormValue("phone")
+
+	query := `
+	INSERT INTO users (name, email, phone)
+	VALUES ($1, $2, $3)
+	`
+
+	_, err = db.Exec(query, name, email, phone)
+	if err != nil {
+		http.Error(w, "Failed to store user data", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Stored user data:", name, email, phone)
+	w.Write([]byte("User data stored successfully"))
+}
+
+// MAIN
+func main() {
+	initDB()
+
+	http.HandleFunc("/", formHandler)
+	http.HandleFunc("/submit", submitHandler)
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
